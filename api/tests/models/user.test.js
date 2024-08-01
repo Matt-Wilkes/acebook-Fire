16be3ac5ac069db1d37290af9e36e43f8a13f3de
annagwozdz <zuza.gwo@gmail.com>
2024-08-01 15:37:55 +0100

    Test user model

--- api/tests/models/user.test.js
+++ api/tests/models/user.test.js
@@ -6,6 +6,21 @@ describe("User model", () => {
     await User.deleteMany({});
   });
 
+  it("has a first name", () => {
+    const user = new User({
+      firstName: "john",
+      email: "someone@example.com",
+      password: "password",
+    });
+    expect(user.firstName).toEqual("john");
+  });
+  it("has a last name", () => {
+    const user = new User({
+      lastName: "smith",
+      password: "password",
+    });
+    expect(user.lastName).toEqual("smith");
+  });
   it("has an email address", () => {
     const user = new User({
       email: "someone@example.com",
@@ -13,6 +28,7 @@ describe("User model", () => {
     });
     expect(user.email).toEqual("someone@example.com");
   });
+
 
   it("has a password", () => {
     const user = new User({
@@ -22,6 +38,17 @@ describe("User model", () => {
     expect(user.password).toEqual("password");
   });
 
+  it("has a confirm password", () => {
+    const user = new User({
+      email: "someone@example.com",
+      password: "password",
+      confirmPassword: "confirmation"
+    });
+    expect(user.confirmPassword).toEqual("confirmation");
+  });
+
+  ////////////
+
   it("can list all users", async () => {
     const users = await User.find();
     expect(users).toEqual([]);
@@ -29,14 +56,20 @@ describe("User model", () => {
 
   it("can save a user", async () => {
     const user = new User({
+      firstName: "john",
+      lastName: "smith",
       email: "someone@example.com",
       password: "password",
+      confirmPassword: "confirmation"
     });
 
     await user.save();
     const users = await User.find();
 
+    expect(users[0].firstName).toEqual("john");
+    expect(users[0].lastName).toEqual("smith");
     expect(users[0].email).toEqual("someone@example.com");
     expect(users[0].password).toEqual("password");
+    expect(users[0].confirmPassword).toEqual("confirmation");
   });
 });
