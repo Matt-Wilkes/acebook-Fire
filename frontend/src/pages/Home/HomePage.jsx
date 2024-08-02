c21d6ceac587edbd20992e82acfc0d2992baea24
Aivaras Voleika <154103843+aivocode@users.noreply.github.com>
2024-08-03 00:10:33 +0100

    Navbar styling using MUI

--- frontend/src/pages/Home/HomePage.jsx
+++ frontend/src/pages/Home/HomePage.jsx
@@ -2,11 +2,17 @@ import { Link } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 import "./HomePage.css";
 
+import Alert from "@mui/material/Alert";
+
 export const HomePage = () => {
   const { state } = useLocation();
   return (
     <div className="home">
-      <div data-testid="_message">{state}</div>
+      {state && (
+        <Alert data-testid="_message" severity="info">
+          {state}
+        </Alert>
+      )}
       <h1>Welcome to Acebook!</h1>
       <Link to="/signup">Sign Up</Link>
       <Link to="/login">Log In</Link>
