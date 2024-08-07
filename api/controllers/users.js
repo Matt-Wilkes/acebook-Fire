5173a143aa73bb7f3642d0bab2f13624d1c86d64
Aivaras Voleika <154103843+aivocode@users.noreply.github.com>
2024-08-07 11:52:08 +0100

    Backend tests + responses fixes for controllers

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,12 +1,8 @@
 const User = require("../models/user");
 
-const object = {
-
-
-}
-
 const create = (req, res) => {
-  const { firstName, lastName, email, password, confirmPassword, image } = req.body;
+  const { firstName, lastName, email, password, confirmPassword, image } =
+    req.body;
 
   const user = new User({
     firstName,
@@ -29,11 +25,10 @@ const create = (req, res) => {
     });
 };
 
-
 const getAllUsers = async (req, res) => {
   const users = await User.find();
   res.status(200).json({ users: users });
-}
+};
 
 // const getUser = async (req, res) => {
 //   const userId = req.params.userId
@@ -45,27 +40,32 @@ const findUserById = async (req, res) => {
   // console.log(req.headers.user_id); // <-- this is what comes from frontend (user_id extracted from JWT token)
   const user = await User.findOne({ _id: req.headers.user_id });
   // console.log(user);
-  res.status(200).json({
-    user_id: user["_id"].toString(),
-    firstName: user["firstName"],
-    lastName: user["lastName"],
-    email: user["email"],
-    city: user["city"],
-    bio: user["bio"],
-    image: user["image"],
-  });
+
+  if (!user) {
+    res.status(400).json({ message: "User not found by id" });
+  } else if (user) {
+    res.status(200).json({
+      user_id: user["_id"].toString(),
+      firstName: user["firstName"],
+      lastName: user["lastName"],
+      email: user["email"],
+      city: user["city"],
+      bio: user["bio"],
+      image: user["image"],
+    });
+  }
 };
 
 const updateUserById = async (req, res) => {
   // console.log(req.body);
   if (!req.body.city) {
-    req.body.city = "No city added"
+    req.body.city = "No city added";
   }
   if (!req.body.bio) {
-    req.body.bio = "No bio added"
+    req.body.bio = "No bio added";
   }
 
-  await User.findOneAndUpdate(
+  const updatedUser = await User.findOneAndUpdate(
     { _id: req.body.user_id },
     {
       $set: {
@@ -76,11 +76,14 @@ const updateUserById = async (req, res) => {
         image: req.body.image,
       },
     },
-    { new: false }
+    { new: true }
   );
 
-  res.status(201).json({ message: "Profile updated" });
-
+  if (!updatedUser) {
+    res.status(400).json({ message: "User not found by id, and not updated" });
+  } else if (updatedUser) {
+    res.status(201).json({ message: "Profile updated" });
+  }
 };
 
 const UsersController = {
@@ -88,8 +91,7 @@ const UsersController = {
   getAllUsers: getAllUsers,
   // getUser: getUser,
   findUserById: findUserById,
-  updateUserById: updateUserById
-
+  updateUserById: updateUserById,
 };
 
 module.exports = UsersController;
--- api/tests/controllers/users.test.js
+++ api/tests/controllers/users.test.js
@@ -5,28 +5,49 @@ const User = require("../../models/user");
 
 require("../mongodb_helper");
 
+// Setup token so we can put it in Authorization
+const secret = process.env.JWT_SECRET;
+const JWT = require("jsonwebtoken");
+const createToken = (userId) => {
+  return JWT.sign(
+    {
+      user_id: userId,
+      iat: Math.floor(Date.now() / 1000) - 5 * 60,
+    },
+    secret
+  );
+};
+let token;
+
 describe("/users", () => {
   beforeEach(async () => {
     await User.deleteMany({});
   });
 
-  const oldConsoleError = console.error
-  console.error = () => {}
-
+  const oldConsoleError = console.error;
+  console.error = () => {};
 
   describe("POST, when email and password are provided", () => {
     test("the response code is 201", async () => {
-      const response = await request(app)
-        .post("/users")
-        .send({ firstName: "john", lastName: "smith", email: "poppy@email.com", password: "1234", confirmPassword: "1234" });
+      const response = await request(app).post("/users").send({
+        firstName: "john",
+        lastName: "smith",
+        email: "poppy@email.com",
+        password: "1234",
+        confirmPassword: "1234",
+      });
 
       expect(response.statusCode).toBe(201);
     });
 
     test("a user is created", async () => {
-      await request(app)
-        .post("/users")
-        .send({ firstName: "john", lastName: "smith", email: "poppy@email.com", password: "1234", confirmPassword: "1234" });
+      await request(app).post("/users").send({
+        firstName: "john",
+        lastName: "smith",
+        email: "poppy@email.com",
+        password: "1234",
+        confirmPassword: "1234",
+      });
 
       const users = await User.find();
       const newUser = users[users.length - 1];
@@ -52,7 +73,7 @@ describe("/users", () => {
 
       const users = await User.find();
       // expect(users.length).toEqual(0);
-      oldConsoleError()
+      oldConsoleError();
     });
   });
 
@@ -63,7 +84,7 @@ describe("/users", () => {
         .send({ password: "1234" });
 
       // expect(response.statusCode).toBe(400);
-      oldConsoleError()
+      oldConsoleError();
     });
 
     test("does not create a user", async () => {
@@ -71,6 +92,131 @@ describe("/users", () => {
 
       const users = await User.find();
       expect(users.length).toEqual(0);
+    });
+  });
+});
+
+describe("/users/get-user", () => {
+  beforeEach(async () => {
+    await User.deleteMany({});
+  });
+
+  describe("GET, with user_id provided", () => {
+    test("response code 200, response body", async () => {
+      const mockUser = new User({
+        firstName: "Potato",
+        lastName: "Tomato",
+        email: "clarkson@farm.com",
+        password: "12345",
+        confirmPassword: "12345",
+        city: "London",
+        bio: "Eat, sleep, work, repeat",
+      });
+
+      const user = await mockUser.save();
+      const mockUserId = user["_id"].toString();
+
+      const response = await request(app)
+        .get("/users/get-user")
+        .set("user_id", mockUserId);
+
+      expect(response.statusCode).toBe(200);
+      expect(response.body).toStrictEqual({
+        user_id: mockUserId,
+        firstName: "Potato",
+        lastName: "Tomato",
+        email: "clarkson@farm.com",
+        city: "London",
+        bio: "Eat, sleep, work, repeat",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541",
+      });
+    });
+
+    test("response code 400", async () => {
+      const response = await request(app)
+        .get("/users/get-user")
+        .set("user_id", "66b2ab160e16d154d1022cd0");
+
+      expect(response.statusCode).toBe(400);
+      expect(response.body).toStrictEqual({ message: "User not found by id" });
+    });
+  });
+});
+
+describe("/users/update-user", () => {
+  beforeEach(async () => {
+    await User.deleteMany({});
+  });
+
+  describe("PUT, with user_id provided", () => {
+    test("response code 201, response body", async () => {
+      const mockUser = new User({
+        firstName: "Potato",
+        lastName: "Tomato",
+        email: "clarkson@farm.com",
+        password: "12345",
+        confirmPassword: "12345",
+        city: "London",
+        bio: "Eat, sleep, work, repeat",
+      });
+
+      const user = await mockUser.save();
+      token = createToken(user.id);
+      const mockUserId = user["_id"].toString();
+
+      // console.log(mockUserId);
+      // console.log(token);
+      const response = await request(app)
+        .put("/users/update-user")
+        .set("Authorization", `Bearer ${token}`)
+        .send({
+          user_id: mockUserId,
+          firstName: "Potato-Edit",
+          lastName: "Tomato-Edit",
+          city: "London-Edit",
+          bio: "Eat, sleep, work, repeat-Edit",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541-Edit",
+        });
+
+      expect(response.statusCode).toBe(201);
+      expect(response.body).toStrictEqual({ message: "Profile updated" });
+    });
+
+    test("response code 400, response body", async () => {
+      const mockUser = new User({
+        firstName: "Potato",
+        lastName: "Tomato",
+        email: "clarkson@farm.com",
+        password: "12345",
+        confirmPassword: "12345",
+        city: "London",
+        bio: "Eat, sleep, work, repeat",
+      });
+
+      const user = await mockUser.save();
+      token = createToken(user.id);
+      const mockUserId = user["_id"].toString();
+
+      // 66b2ab160e16d154d1022cd0 <== mock correct format _id for User
+      const response = await request(app)
+        .put("/users/update-user")
+        .set("Authorization", `Bearer ${token}`)
+        .send({
+          user_id: "66b2ab160e16d154d1022cd0",
+          firstName: "Potato-Edit",
+          lastName: "Tomato-Edit",
+          city: "London-Edit",
+          bio: "Eat, sleep, work, repeat-Edit",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541-Edit",
+        });
+
+      expect(response.statusCode).toBe(400);
+      expect(response.body).toStrictEqual({
+        message: "User not found by id, and not updated",
+      });
     });
   });
 });
