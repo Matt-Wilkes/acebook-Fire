78fa61daf8a047a578e5269106f3acdad4289663
Aivaras Voleika <154103843+aivocode@users.noreply.github.com>
2024-08-08 00:03:41 +0100

    Simplified LogoutButton test

--- frontend/tests/components/LogoutButton.test.jsx
+++ frontend/tests/components/LogoutButton.test.jsx
@@ -1,79 +1,83 @@
-import { screen, render, fireEvent, cleanup } from "@testing-library/react";
+import { screen, render, fireEvent } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
-import { expect, afterEach } from "vitest";
+import { expect, beforeEach } from "vitest";
 import "@testing-library/jest-dom/vitest";
-import { RouterProvider, createMemoryRouter } from "react-router-dom";
 
 import Context from "../../src/components/Context/Context";
-import Navbar from "../../src/components/Navbar/Navbar";
+import Navbar from "../../src/components/Navbar/Navbar"
 
-import routesConfig from "../_helpers/routesConfig";
-
-afterEach(() => {
-  cleanup();
-});
-
-// Variable and function to pass data to Context.Provider to mock behaviour of useState in App.jsx
+// Mock authentication
 let authStatus = false;
 const setAuthStatus = (newValue) => {
   authStatus = newValue;
 };
 
-// Router equivalent to router implemented in App.jsx
-const router = createMemoryRouter(routesConfig, {
-  initialEntries: ["/"],
-});
+describe("MyProfile.jsx", () => {
+  beforeEach(() => {
+    window.localStorage.removeItem("token");
+    setAuthStatus(false);
+  });
 
-test("renders Logout button element when logged in", () => {
-  setAuthStatus(true);
-  render(
-    <Context.Provider value={{ authStatus, setAuthStatus }}>
-      <BrowserRouter>
-        <Navbar />
-      </BrowserRouter>
-    </Context.Provider>
-  );
+  test("renders Logout button element when logged in", () => {
+    setAuthStatus(true);
+    render(
+      <Context.Provider value={{ authStatus, setAuthStatus }}>
+        <BrowserRouter>
+          <Navbar />
+        </BrowserRouter>
+      </Context.Provider>
+    );
 
-  const nav = screen.queryByTestId("_nav");
-  expect(nav).toBeInTheDocument();
+    const nav = screen.queryByTestId("_nav");
+    expect(nav).toBeInTheDocument();
 
-  const logout = screen.queryByTestId("_logout");
-  expect(logout).toBeInTheDocument();
-});
+    const logout = screen.queryByTestId("_logout");
+    expect(logout).toBeInTheDocument();
+  });
 
-test("does not render Logout button element when logged out", () => {
-  setAuthStatus(false);
-  render(
-    <Context.Provider value={{ authStatus, setAuthStatus }}>
-      <BrowserRouter>
-        <Navbar />
-      </BrowserRouter>
-    </Context.Provider>
-  );
+  test("does not render Logout button element when logged out", () => {
+    setAuthStatus(false);
+    render(
+      <Context.Provider value={{ authStatus, setAuthStatus }}>
+        <BrowserRouter>
+          <Navbar />
+        </BrowserRouter>
+      </Context.Provider>
+    );
 
-  const nav = screen.queryByTestId("_nav");
-  expect(nav).toBeInTheDocument();
+    const nav = screen.queryByTestId("_nav");
+    expect(nav).toBeInTheDocument();
 
-  const logout = screen.queryByTestId("_logout");
-  expect(logout).not.toBeInTheDocument();
-});
+    const logout = screen.queryByTestId("_logout");
+    expect(logout).not.toBeInTheDocument();
+  });
 
-test("clicking Logout button redirects to HomePage with message", async () => {
-  setAuthStatus(true);
-  render(
-    <Context.Provider value={{ authStatus, setAuthStatus }}>
-      <RouterProvider router={router} />
-    </Context.Provider>
-  );
+  test("clicking Logout button will delete token", async () => {
+    window.localStorage.setItem(
+      "token",
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjZhZThmNmFjYzNiNzc1NDJlYTc0ZDIxIiwiaWF0IjoxNzIyODk1OTcwfQ.QOeRf8AgdnpPEiD_51hF73W9WW2c87Z5v_ZVTo6riP8"
+    );
 
-  expect(screen.getByTestId("_nav")).toBeInTheDocument();
+    setAuthStatus(true);
 
-  const logout = screen.queryByTestId("_logout");
-  expect(logout).toBeInTheDocument();
+    render(
+      <Context.Provider value={{ authStatus, setAuthStatus }}>
+        <BrowserRouter>
+          <Navbar />
+        </BrowserRouter>
+      </Context.Provider>
+    );
 
-  fireEvent.click(logout);
+    expect(screen.getByTestId("_nav")).toBeInTheDocument();
 
-  const message = screen.queryByTestId("_message");
-  expect(message).toBeInTheDocument();
-  expect(message).toHaveTextContent("You were logged out.");
+    const logout = screen.queryByTestId("_logout");
+    expect(logout).toBeInTheDocument();
+
+    expect(localStorage.getItem("token")).toBe("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjZhZThmNmFjYzNiNzc1NDJlYTc0ZDIxIiwiaWF0IjoxNzIyODk1OTcwfQ.QOeRf8AgdnpPEiD_51hF73W9WW2c87Z5v_ZVTo6riP8");
+
+    fireEvent.click(logout);
+
+    expect(authStatus).toBe(false);
+    expect(localStorage.getItem("token")).toBe(null);
+  });
 });
